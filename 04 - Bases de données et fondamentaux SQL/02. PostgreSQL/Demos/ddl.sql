177c8bae696ca143016dc79d0fd1d1ec25957229
Christophe Ringot <christophe.r@utopios.solutions>
2024-07-11 16:54:45 +0200

    SQL - fin exercices 6-7-8 merise et debut demo DDLpostgreSQL

---
+++ 04 - Bases de données et fondamentaux SQL/02. PostgreSQL/Demos/ddl.sql
@@ -0,0 +1,64 @@
+-- DDL : DATA DEFINITION LANGUAGE
+-- Pour faire un commantaire, on utilise deux "-" suivi d'un espace.
+
+-- Choisir le schéma à utiliser.
+SET search_path TO entreprise_demo;
+
+-- Création d'une base de données :
+-- CREATE DATABASE demo01;
+
+-- Suppression d'une base de données : 
+-- DROP DATABASE demo01;
+
+-- CREATE <type objet> <nom>;
+-- IF NOT EXISTS : Création de la BDD uniquement si elle n'existe pas !
+CREATE SCHEMA IF NOT EXISTS entreprise_demo;
+
+-- Suppression du schéma
+-- DROP SCHEMA IF EXISTS entreprise_demo;
+
+----------------------------------------
+------------ CREATE TABLE --------------
+----------------------------------------
+
+-- Pour créer une table, on utilise l'instruction CREATE TABLE
+-- On peut vérifier que la table n'existe pas avant de la créer
+-- Pour être sûr de créer la table au bon endroit, on peut préfixer du schéma
+CREATE TABLE IF NOT EXISTS services (
+	-- <nom de l'attribut> <type> <contraintes>
+	service_id SERIAL PRIMARY KEY,
+	libelle VARCHAR(50)
+);
+
+-- Suppression d'une table si elle existe :
+-- DROP TABLE IF EXISTS entreprise_demo.services;
+
+
+CREATE TABLE IF NOT EXISTS salarie (
+	-- Fait la même chose que SERIAL, c'est à dire créer un Id unique automatiquement
+	-- Respecte au mieux la convention SQL
+	salarie_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	-- On précise NOT NULL, par défaut la valeur peut être null
+	prenom VARCHAR(50) NOT NULL,
+	nom VARCHAR(50) NOT NULL,
+	-- La contrainte check permet d'ajouter un méchanisme de vérification
+	"age" INT NOT NULL CHECK("age" > 18 AND "age" < 65),
+	-- La contrainte default permet de définir une valeur par défaut en cas d'absence de valeur renseigné
+	salaire DECIMAL(6,2) DEFAULT 1500,
+	service_id INT NOT NULL,
+	-- Contrainte qui permet de lier la colonne service_id à la table à laquelle elle fait référence
+	CONSTRAINT fk_service_id FOREIGN KEY(service_id) 
+	REFERENCES entreprise_demo.services(service_id)
+);
+
+-- Modification d'une table
+-- ALTER TABLE entreprise_demo.salarie RENAME TO salaries;
+
+-- Suppression d'une colonne
+ALTER TABLE salaries
+	DROP COLUMN salaire;
+
+-- Ajout d'une colonne
+ALTER TABLE salaries
+	ADD COLUMN salaire DECIMAL(6,2) DEFAULT 1500;
+
---
+++ 04 - Bases de données et fondamentaux SQL/02. PostgreSQL/Exercices/Sujets/exercice_01.md
@@ -0,0 +1,12 @@
+# Création d'une base de données
+
+## Objectif:
+ - Création d'une table
+
+1. Créer une base de données pour l'exercice : `exercice_01`
+2. Créer une table personne avec les champs suivants :
+    - id
+    - prenom
+    - nom
+    - email
+    - telephone
