235f1ba9bb83217119aedcc2a9378d9a9da5e4d4
Léo Lhomme <leo@utopios.solutions>
2024-07-29 12:01:11 +0200

    Java Poo _ support exo 2

--- 08 - JAVA/01 - POO/Demo/Demo/src/main/java/org/example/Demo1/Voiture.java
+++ 08 - JAVA/01 - POO/Demo/Demo/src/main/java/org/example/Demo1/Voiture.java
@@ -1,5 +1,10 @@
 package org.example.Demo1;
 
+
+/**
+ * Classe voiture
+ *
+ */
 public class Voiture {
     private String model;
     private String couleur;
@@ -7,14 +12,37 @@ public class Voiture {
     private int autonomie;
     private boolean demaree;
 
-    public Voiture() { }
+    private static int nombreVoiture = 0;
+
+    public Voiture() {
+        nombreVoiture++;
+    }
+
+    /**
+     * constructeur de la voiture
+     * @param model model de la voiture
+     * @param couleur couleur de la voiture
+     * @param reservoir contenance du reservoire de la voiture
+     * @param autonomie autonomie de la voiture
+     */
+
+    // Mon constructeur voiture
     public Voiture(String model, String couleur, int reservoir, int autonomie) {
+        this(reservoir,autonomie);
         this.model = model;
         this.couleur = couleur;
+    }
+
+    public Voiture( int reservoir, int autonomie){
+        this();
         this.reservoir = reservoir;
         this.autonomie = autonomie;
     }
 
+
+    /**
+     * methode d'affichage de la voiture
+     */
     public void afficher (){
         System.out.println("Notre première voiture est une " + this.model + " de couleur " + this.couleur);
         System.out.println("Elle a un réservoir de " + this.reservoir + " litres pour une autonomie de " + this.autonomie + " km.");
@@ -27,6 +55,10 @@ public class Voiture {
             System.out.println("le moteur demare");
             demaree = true;
         }
+    }
+
+    public static int getNombreVoiture (){
+      return nombreVoiture;
     }
     public String getModel (){
         return this.model;
