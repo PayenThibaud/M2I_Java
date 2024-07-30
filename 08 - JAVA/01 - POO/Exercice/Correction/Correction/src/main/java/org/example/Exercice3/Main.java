b8a3d00760f9cac3b43d2ca242a172fb7f49a060
Léo Lhomme <leo@utopios.solutions>
2024-07-30 09:15:14 +0200

    Java Poo _ correction exercice3 WaterTanks

--- 08 - JAVA/01 - POO/Exercice/Correction/Correction/src/main/java/org/example/Exercice3/Main.java
+++ 08 - JAVA/01 - POO/Exercice/Correction/Correction/src/main/java/org/example/Exercice3/Main.java
@@ -2,6 +2,6 @@ package org.example.Exercice3;
 
 public class Main {
     public static void main(String[] args) {
-        WaterTank waterTank = new WaterTank(2000.90f,20000);
+        new IHM().startMenu();
     }
 }
--- 08 - JAVA/01 - POO/Exercice/Correction/Correction/src/main/java/org/example/Exercice3/WaterTank.java
+++ 08 - JAVA/01 - POO/Exercice/Correction/Correction/src/main/java/org/example/Exercice3/WaterTank.java
@@ -9,6 +9,8 @@ public class WaterTank {
      */
     private int remplissage = 0;
 
+    private static int quantiteTotalReservoires;
+
     public WaterTank (float poidVide,int capaciteTotal){
         this.poidVide = poidVide;
         this.capaciteTotal = capaciteTotal;
@@ -25,6 +27,148 @@ public class WaterTank {
         return poidVide + remplissage;
     }
 
+    private void setRemplissage (int quantite){
+        quantiteTotalReservoires -= this.remplissage;
+        this.remplissage  = quantite;
+        quantiteTotalReservoires += remplissage;
+    }
+
+    public int remplir(int quantite){
+        if(quantite<0){
+            quantite = 0;
+        }
+
+        if(remplissage+quantite <= capaciteTotal){
+            setRemplissage(remplissage+ quantite);
+            return 0;
+        }
+        else{
+            int excedent = remplissage + quantite - capaciteTotal;
+            setRemplissage(capaciteTotal);
+            return excedent;
+        }
+    }
+
+    public int vider (int quantite){
+        if(quantite < 0){
+            quantite = 0;
+        }
+
+        if(remplissage - quantite < 0){
+            int eauRecup = remplissage;
+            setRemplissage(0);
+            return eauRecup;
+        }else{
+            setRemplissage(remplissage - quantite);
+            return quantite;
+        }
+    }
+
+    public static int getAllQuatityFronWaterTank(){
+        return quantiteTotalReservoires;
+    }
+
+    public void affichageCiterne()
+    {
+        String[][] citerneAscii = {
+                {"┌──────O─────┐",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "└────────────┘"},
+                {"┌──────O─────┐",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│~~~~~~~~~~~~│",
+                        "└────────────┘"},
+                {"┌──────O─────┐",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│~~~~~~~~~~~~│",
+                        "│            │",
+                        "└────────────┘"},
+                {"┌──────O─────┐",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│~~~~~~~~~~~~│",
+                        "│            │",
+                        "│            │",
+                        "└────────────┘"},
+                {"┌──────O─────┐",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│~~~~~~~~~~~~│",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "└────────────┘"},
+                {"┌──────O─────┐",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│~~~~~~~~~~~~│",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "└────────────┘"},
+                {"┌──────O─────┐",
+                        "│            │",
+                        "│            │",
+                        "│~~~~~~~~~~~~│",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "└────────────┘"},
+                {"┌──────O─────┐",
+                        "│            │",
+                        "│~~~~~~~~~~~~│",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "└────────────┘"},
+                {"┌──────O─────┐",
+                        "│~~~~~~~~~~~~│",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "│            │",
+                        "└────────────┘"},
+        };
+        int niveauCiterne = this.remplissage == 1 ? 1 : Math.round((float) (this.remplissage * 8) / this.capaciteTotal);
+        System.out.println(niveauCiterne);
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(citerneAscii[niveauCiterne][i]);
+        }
+    }
 
 
 
