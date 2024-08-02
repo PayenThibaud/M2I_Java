383a86f9ddd29cf2b645229d1a735dd0c780f801
Léo Lhomme <leo@utopios.solutions>
2024-08-02 14:16:03 +0200

    Java Poo _ correction exercice exception 1

---
+++ 08 - JAVA/01 - POO/Exercice/Correction/Correction/src/main/java/org/example/ExerciceException/Exo1.java
@@ -0,0 +1,20 @@
+package org.example.ExerciceException;
+
+import java.util.Scanner;
+
+public class Exo1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            try{
+                System.out.println("Entrer une valeur numérique");
+                String entryStr = scanner.nextLine();
+                int entry = Integer.parseInt(entryStr);
+                System.out.println("la valeur est : "+entry);
+                break;
+            }catch (NumberFormatException e){
+                System.out.println("La valeur doit etre un entier !");
+            }
+        }
+    }
+}
