7621552ac4a64d5fc8c7a08705701d1be985b020
Leo Lhomme <leo@utopios.solutions>
2024-08-20 16:43:35 +0200

    Java Design Patern _ demo singleton

---
+++ 08 - JAVA/02 - DESIGN PATTERN/Demo/demoDessignParttern/src/main/java/org/example/demo_singleton/Singleton.java
@@ -0,0 +1,22 @@
+package org.example.demo_singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+    private String urlConnection;
+
+    private Singleton (String urlConnection){
+        this.urlConnection = urlConnection;
+    }
+
+    public String getUrlConnection() {
+        return urlConnection;
+    }
+
+    public static synchronized Singleton getInstance(String url){
+        if(instance == null){
+            instance = new Singleton(url);
+        }
+        return instance;
+    }
+}
