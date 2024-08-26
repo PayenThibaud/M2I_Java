660b95253ed9165f2015d895b341ba4d2a878929
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-08-26 12:19:22 +0200

    exo 1 JPA

---
+++ Thibaud_exo/Java_JPA/src/main/java/org/example/Main.java
@@ -0,0 +1,102 @@
+package org.example;
+
+import org.example.entity.Etudient;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class Main {
+
+    private static EntityManager em;
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Java_JPA");
+        em = emf.createEntityManager();
+
+        Etudient etudient = Etudient.builder()
+                .nom("Titi2")
+                .prenom("Toto2")
+                .classe("6Ee")
+                .build();
+
+
+
+
+//        add(etudient);
+
+        System.out.println(getAll());
+        System.out.println(getById(1));
+
+
+        Etudient etudient2 = Etudient.builder()
+                .nom("Toto")
+                .prenom("Titi")
+                .build();
+        update(1, etudient2);
+
+        System.out.println(getById(1));
+
+        delete(1);
+
+        System.out.println(getAll());
+    }
+
+    public static void add (Etudient etudient) {
+        em.getTransaction().begin();
+        em.persist(etudient);
+        em.getTransaction().commit();
+    }
+
+    public static void update (int id, Etudient etudientUpdate) {
+
+        Etudient etudient = getById(id);
+
+        if (etudient == null) {
+            System.out.println("pas d etudient avec cet id");
+            return;
+        }
+        em.getTransaction().begin();
+
+        etudient.setNom(etudientUpdate.getNom());
+        etudient.setPrenom(etudientUpdate.getPrenom());
+
+        if (etudientUpdate.getClasse() != null) {
+            etudient.setClasse(etudientUpdate.getClasse());
+        }
+
+        em.merge(etudient);
+        em.getTransaction().commit();
+    }
+
+    public static void delete (int id) {
+        Etudient etudient = getById(id);
+        if (etudient == null) {
+            System.out.println("pas d etudient avec cet id");
+            return;
+        }
+        em.getTransaction().begin();
+        em.remove(etudient);
+        em.getTransaction().commit();
+    }
+
+    public static Etudient getById (int id) {
+        Etudient etudient = em.find(Etudient.class, id);
+        if(etudient != null){
+            return etudient;
+        }else {
+            System.out.println("pas d etudient avec cet id");
+            return etudient;
+        }
+    }
+
+    public static List<Etudient> getAll() {
+        TypedQuery<Etudient> query = em.createQuery("select e from Etudient e", Etudient.class);
+        return query.getResultList();
+    }
+
+
+}
---
+++ Thibaud_exo/Java_JPA/src/main/java/org/example/entity/Etudient.java
@@ -0,0 +1,27 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Table(name = "etudient")
+public class Etudient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_etudient")
+    private int id;
+    @Column(nullable = false)
+    private String nom;
+    @Column(nullable = false)
+    private String prenom;
+    private String classe;
+
+}
