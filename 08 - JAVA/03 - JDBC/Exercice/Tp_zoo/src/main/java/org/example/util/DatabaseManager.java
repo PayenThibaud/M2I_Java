4e49a1634bd9b7e74e94d636787cad1f4d7d6f7c
Leo Lhomme <leo@utopios.solutions>
2024-08-26 09:17:29 +0200

    Java Jdbc _ correction tp recipe

--- 08 - JAVA/03 - JDBC/Exercice/Tp_zoo/src/main/java/org/example/util/DatabaseManager.java
+++ 08 - JAVA/03 - JDBC/Exercice/Tp_zoo/src/main/java/org/example/util/DatabaseManager.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseManager {
 
-    private static final String URI = "jdbc:mysql://localhost:3306/demo_jdbc";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Root";
+    private static final String URI = "jdbc:postgresql://localhost:5432/";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "formation";
 
     public static Connection getConnection() throws SQLException{
         Connection connection = DriverManager.getConnection(URI,USER,PASSWORD);
