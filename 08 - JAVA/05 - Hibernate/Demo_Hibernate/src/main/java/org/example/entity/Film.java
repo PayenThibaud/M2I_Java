5afab0d6051ed90a70d5d5a9b6ab25724b70e615
Leo Lhomme <leo@utopios.solutions>
2024-08-28 10:05:21 +0200

    Java Hibernate _ demo hibernate

---
+++ 08 - JAVA/05 - Hibernate/Demo_Hibernate/src/main/java/org/example/entity/Film.java
@@ -0,0 +1,25 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titre;
+    private int duree;
+    private String categorie;
+}
