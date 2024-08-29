539884924ec38db2ebb7ff0099c7cbf76aee2420
Leo Lhomme <leo@utopios.solutions>
2024-08-29 15:08:48 +0200

    JEE _ correction exercice 1

---
+++ 08 - JAVA/06 - JEE/Exercice/Exercice1/src/main/java/JspServlet.java
@@ -0,0 +1,17 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "jspServlet",value = "/jsp")
+public class JspServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        getServletContext().getRequestDispatcher("/WEB-INF/pageJsp.jsp").forward(req,resp);
+    }
+}
---
+++ 08 - JAVA/06 - JEE/Exercice/Exercice1/src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrateur
+  Date: 29/08/2024
+  Time: 14:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h1>Exercice 1</h1>
+
+<a href="text">text</a>
+<a href="html">html</a>
+<a href="jsp">JSP</a>
+
+</body>
+</html>
