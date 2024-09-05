9013c85a0543bec1d3aed060f3062bd7d9e68fda
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-09-05 16:46:13 +0200

    correction assertion exo2

--- Thibaud_exo/Java_TDD_exo/src/test/java/org/example/exo2/FibTest.java
+++ Thibaud_exo/Java_TDD_exo/src/test/java/org/example/exo2/FibTest.java
@@ -14,7 +14,20 @@ public class FibTest {
     private Fib fib;
 
     @Test
-    public void testFibWhen_Range_1_Result_NotVoid_And_0() {
+    public void testFibWhen_Range_1_Result_NotVoid() {
+        // Arrange
+        int range = 1;
+        fib = new Fib(range);
+
+        // Act
+        List<Integer> result = fib.getFibSeries();
+
+        // Assert
+        Assertions.assertTrue(result.size() != 0);
+    }
+
+    @Test
+    public void testFibWhen_Range_1_Result_0() {
         // Arrange
         int range = 1;
         fib = new Fib(range);
@@ -24,28 +37,76 @@ public class FibTest {
         List<Integer> result = fib.getFibSeries();
 
         // Assert
-        Assertions.assertTrue(result.size() != 0);
+
         Assertions.assertEquals(resultAwait, result.get(0));
     }
 
     @Test
-    public void testFibWhen_Range_6_Result_Have_3_Size_6_Donthave_4_Sorted() {
+    public void testFibWhen_Range_6_Result_Size_6() {
         // Arrange
         int range = 6;
         Fib fib = new Fib(range);
-        int no4 = 4;
-        List<Integer> resultAwait = Arrays.asList(0, 1, 1, 2, 3, 5);
-        int have3 = 3;
-        List<Integer> resultTrierAwait = fib.getFibSeries().stream().sorted().collect(Collectors.toList());
 
         // Act
         List<Integer> result = fib.getFibSeries();
 
         // Assert
         Assertions.assertEquals(range, result.size());
+    }
+
+    @Test
+    public void testFibWhen_Range_6_Result_Have_3() {
+        // Arrange
+        int range = 6;
+        Fib fib = new Fib(range);
+        int have3 = 3;
+
+        // Act
+        List<Integer> result = fib.getFibSeries();
+
+        // Assert
         Assertions.assertTrue(result.contains(have3));
+    }
+
+    @Test
+    public void testFibWhen_Range_6_Result_Donthave_4() {
+        // Arrange
+        int range = 6;
+        Fib fib = new Fib(range);
+        int no4 = 4;
+
+        // Act
+        List<Integer> result = fib.getFibSeries();
+
+        // Assert
         Assertions.assertFalse(result.contains(no4));
-        Assertions.assertEquals(resultAwait, result);
+    }
+
+    @Test
+    public void testFibWhen_Range_6_Result_Sorted() {
+        // Arrange
+        int range = 6;
+        Fib fib = new Fib(range);
+        List<Integer> resultTrierAwait = fib.getFibSeries().stream().sorted().collect(Collectors.toList());
+
+        // Act
+        List<Integer> result = fib.getFibSeries();
+
+        // Assert
         Assertions.assertEquals(resultTrierAwait, result);
+    }
+
+    @Test
+    public void testFibWhen_Range_6_Result_HaveList_0_1_1_2_3_5() {
+        // Arrange
+        int range = 6;
+        Fib fib = new Fib(range);
+        List<Integer> resultAwait = Arrays.asList(0, 1, 1, 2, 3, 5);
+
+        // Act
+        List<Integer> result = fib.getFibSeries();
+
+        // Assert
+        Assertions.assertEquals(resultAwait, result);
     }
 }
