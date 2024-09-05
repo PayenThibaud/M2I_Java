0188cfe6fd6864f22c5a1a069594cdea601a532b
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-09-05 13:52:33 +0200

    exo tdd

---
+++ Thibaud_exo/Java_TDD_exo/src/main/java/org/example/GradingCalculator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class GradingCalculator {
+    public int score;
+    public int attendancePercentage;
+
+    public GradingCalculator(int score, int attendancePercentage) {
+        this.score = score;
+        this.attendancePercentage = attendancePercentage;
+    }
+
+    public char getGrade() {
+        if(score > 90 && attendancePercentage > 70) return 'O';
+        else if(score > 80 && attendancePercentage > 60) return  'O';
+        else if (score > 60 && attendancePercentage > 60) return  'O';
+        else return 'O';
+    }
+}
---
+++ Thibaud_exo/Java_TDD_exo/src/test/java/org/example/GradingCalculatorTest.java
@@ -0,0 +1,121 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GradingCalculatorTest {
+
+    private GradingCalculator gradingCalculator;
+
+    @Test
+    public void TestGradingCalculatorWhenScore_50_Presence_90_Resultat_F() {
+        // Arrange
+        int score = 50;
+        int presence = 90;
+        gradingCalculator = new GradingCalculator(score, presence);
+        char resultAwait = 'F';
+
+        // Act
+        char result = gradingCalculator.getGrade();
+
+        //Assert
+        Assertions.assertEquals(resultAwait, result);
+
+    }
+
+    @Test
+    public void TestGradingCalculatorWhenScore_65_Presence_55_Resultat_F() {
+        // Arrange
+        int score = 65;
+        int presence = 55;
+        gradingCalculator = new GradingCalculator(score, presence);
+        char resultAwait = 'F';
+
+        // Act
+        char result = gradingCalculator.getGrade();
+
+        //Assert
+        Assertions.assertEquals(resultAwait, result);
+
+    }
+
+    @Test
+    public void TestGradingCalculatorWhenScore_95_Presence_55_Resultat_F() {
+        // Arrange
+        int score = 95;
+        int presence = 55;
+        gradingCalculator = new GradingCalculator(score, presence);
+        char resultAwait = 'F';
+
+        // Act
+        char result = gradingCalculator.getGrade();
+
+        //Assert
+        Assertions.assertEquals(resultAwait, result);
+
+    }
+
+    @Test
+    public void TestGradingCalculatorWhenScore_95_Presence_65_Resultat_B() {
+        // Arrange
+        int score = 95;
+        int presence = 65;
+        gradingCalculator = new GradingCalculator(score, presence);
+        char resultAwait = 'B';
+
+        // Act
+        char result = gradingCalculator.getGrade();
+
+        //Assert
+        Assertions.assertEquals(resultAwait, result);
+
+    }
+
+    @Test
+    public void TestGradingCalculatorWhenScore_65_Presence_90_Resultat_C() {
+        // Arrange
+        int score = 65;
+        int presence = 90;
+        gradingCalculator = new GradingCalculator(score, presence);
+        char resultAwait = 'C';
+
+        // Act
+        char result = gradingCalculator.getGrade();
+
+        //Assert
+        Assertions.assertEquals(resultAwait, result);
+
+    }
+
+    @Test
+    public void TestGradingCalculatorWhenScore_85_Presence_90_Resultat_B() {
+        // Arrange
+        int score = 85;
+        int presence = 90;
+        gradingCalculator = new GradingCalculator(score, presence);
+        char resultAwait = 'B';
+
+        // Act
+        char result = gradingCalculator.getGrade();
+
+        //Assert
+        Assertions.assertEquals(resultAwait, result);
+
+    }
+
+    @Test
+    public void TestGradingCalculatorWhenScore_95_Presence_90_Resultat_A() {
+        // Arrange
+        int score = 95;
+        int presence = 90;
+        gradingCalculator = new GradingCalculator(score, presence);
+        char resultAwait = 'A';
+
+        // Act
+        char result = gradingCalculator.getGrade();
+
+        //Assert
+        Assertions.assertEquals(resultAwait, result);
+
+    }
+}
