91416c3f778a6e7b73dc0dab09da2a0db516977b
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-09-06 13:35:08 +0200

    exo 4

--- Thibaud_exo/Java_TDD_exo/src/main/java/org/example/exo4/RechercheVille.java
+++ Thibaud_exo/Java_TDD_exo/src/main/java/org/example/exo4/RechercheVille.java
@@ -4,13 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RechercheVille {
-    private List<String> villes;
+    private List<String> villes = Arrays.asList("Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul");
 
 
     public List<String> rechercher(String mot)  {
-        if(mot.length()<2){
+        if(mot.length() < 2){
             throw new NotFoundException();
         }
+
+        List<String> villesRecherche = villes.stream().filter(v);
     return null;
     }
 }
