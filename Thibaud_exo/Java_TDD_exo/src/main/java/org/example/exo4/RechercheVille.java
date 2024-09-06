37ba940d71d7ae4ac129947a7528ef5feef17c3a
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-09-06 15:54:18 +0200

    ajout methode

--- Thibaud_exo/Java_TDD_exo/src/main/java/org/example/exo4/RechercheVille.java
+++ Thibaud_exo/Java_TDD_exo/src/main/java/org/example/exo4/RechercheVille.java
@@ -4,15 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RechercheVille {
-    private List<String> villes = Arrays.asList("Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul");
+    private List<String> villes = Arrays.asList("Paris","Lille", "Lyon", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul");
 
 
     public List<String> rechercher(String mot)  {
+
+        if(mot.equals("*")){
+            return villes;
+        }
+
+
         if(mot.length() < 2){
             throw new NotFoundException();
         }
 
-        List<String> villesRecherche = villes.stream().filter(v);
-    return null;
+        List<String> villesRecherche = villes.stream().filter(v -> v.toUpperCase().contains(mot.toUpperCase())).toList();
+    return villesRecherche;
     }
 }
