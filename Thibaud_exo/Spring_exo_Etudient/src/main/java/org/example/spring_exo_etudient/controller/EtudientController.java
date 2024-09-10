50747b5a1d04d64e3e9fd9eaadb9f6809bd135ab
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-09-10 10:06:49 +0200

    maj exo etudient spring

--- Thibaud_exo/Spring_exo_Etudient/src/main/java/org/example/spring_exo_etudient/controller/EtudientController.java
+++ Thibaud_exo/Spring_exo_Etudient/src/main/java/org/example/spring_exo_etudient/controller/EtudientController.java
@@ -1,10 +1,15 @@
 package org.example.spring_exo_etudient.controller;
 
+import org.example.spring_exo_etudient.model.Etudient;
 import org.example.spring_exo_etudient.service.EtudientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class EtudientController {
@@ -17,7 +22,14 @@ public class EtudientController {
 
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("EtudientsName",etudientService.getEtudients());
+        model.addAttribute("EtudientsNom",etudientService.getEtudients());
+        return "index";
+    }
+
+    @RequestMapping("/recherche")
+    public String search(@RequestParam("nom") String nom, Model model) {
+        List<Etudient> resultatsRecherche = etudientService.getEtudientsByName(nom);
+        model.addAttribute("EtudientsNom", resultatsRecherche);
         return "index";
     }
 
@@ -33,4 +45,26 @@ public class EtudientController {
         model.addAttribute("Etudients", etudientService.getEtudientsByName(name));
         return "etudient";
     }
+
+    @RequestMapping( "/inscription")
+    public String inscription(Model model) {
+        Etudient etudientVide = new Etudient("Entrez le nom", "Entrez le prénom", "Entrez l'email", 0);
+        model.addAttribute("Etudient", etudientVide);
+        return "inscription";
+    }
+
+    @RequestMapping(value = "/inscription", method = RequestMethod.POST)
+    public String inscription(
+            @RequestParam String prenom,
+            @RequestParam String nom,
+            @RequestParam String email,
+            @RequestParam int age,
+            Model model) {
+
+        Etudient etudient = new Etudient(nom, prenom, email, age);
+        etudientService.setEtudients(etudient);
+        return "redirect:/";
+    }
+
+
 }
