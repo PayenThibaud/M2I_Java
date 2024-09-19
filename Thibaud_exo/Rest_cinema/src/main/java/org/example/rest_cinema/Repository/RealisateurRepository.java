9d1b1cea1f8ebde494a35128a0d262140339200a
Thibaud Payen <149626091+PayenThibaud@users.noreply.github.com>
2024-09-19 14:18:06 +0200

    ajout des repository pour exo cinema

---
+++ Thibaud_exo/Rest_cinema/src/main/java/org/example/rest_cinema/Repository/RealisateurRepository.java
@@ -0,0 +1,7 @@
+package org.example.rest_cinema.Repository;
+
+import org.example.rest_cinema.entity.Realisateur;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RealisateurRepository extends CrudRepository<Realisateur, Integer> {
+}
