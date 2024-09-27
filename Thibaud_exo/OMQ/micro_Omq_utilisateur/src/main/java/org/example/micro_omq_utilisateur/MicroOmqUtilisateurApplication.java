f0cc0acef49ad8759ce95931e9be15f72b769ffc
Thibaud Payen <149626091+PayenThibaud@users.noreply.github.com>
2024-09-27 13:54:00 +0200

    docker

---
+++ Thibaud_exo/OMQ/micro_Omq_utilisateur/src/main/java/org/example/micro_omq_utilisateur/MicroOmqUtilisateurApplication.java
@@ -0,0 +1,13 @@
+package org.example.micro_omq_utilisateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroOmqUtilisateurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroOmqUtilisateurApplication.class, args);
+    }
+
+}
---
+++ Thibaud_exo/OMQ/micro_Omq_utilisateur/src/test/java/org/example/micro_omq_utilisateur/MicroOmqUtilisateurApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.micro_omq_utilisateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroOmqUtilisateurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
