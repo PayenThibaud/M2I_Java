4c4e556da703153cb7489c0e81a4b5530507fa9f
Thibaud Payen <149626091+PayenThibaud@users.noreply.github.com>
2024-09-27 15:27:44 +0200

    docker

--- Thibaud_exo/Docker_micro/back_employee/src/main/java/org/employee/back_employee/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.employee.back_employee.repository;
-
-import org.employee.back_employee.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
