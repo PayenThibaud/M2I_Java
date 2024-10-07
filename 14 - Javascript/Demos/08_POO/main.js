9d3829126c07d69096e819abf17e6585e74eb4da
Christophe D. <christophe.delory@labom2iformation.fr>
2024-10-07 16:52:37 +0200

    Javascript - fonctions tableaux objet bases debut POO

---
+++ 14 - Javascript/Demos/08_POO/main.js
@@ -0,0 +1,4 @@
+import {  MY_SERCRET_KEY } from "./secret_key.js";
+
+console.log(MY_SERCRET_KEY)
+
---
+++ 14 - Javascript/Demos/08_POO/secret_key.js
@@ -0,0 +1,8 @@
+const MY_SERCRET_KEY = 1234;
+
+const maFonction = function(){
+    return "my super function"
+}
+
+
+export { MY_SERCRET_KEY, maFonction}
