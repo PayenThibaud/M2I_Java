f467d1c12c72df35225d20b159635ad8b47074d5
Christophe D. <christophe.delory@labom2iformation.fr>
2024-10-08 09:52:31 +0200

    demo POO

--- 14 - Javascript/Demos/08_POO/main.js
+++ 14 - Javascript/Demos/08_POO/main.js
@@ -1,4 +1,77 @@
-import {  MY_SERCRET_KEY } from "./secret_key.js";
+import { Contact } from "./classe/contact.js";
+import maVaribale, {  MY_SERCRET_KEY as myKey } from "./secret_key.js";
 
-console.log(MY_SERCRET_KEY)
+console.log(myKey)
 
+console.log(maVaribale)
+
+console.clear()
+
+class Person {
+
+    static count = 0;
+
+    constructor(name){
+        this.name = name;
+        Person.count++;
+    }
+
+    hello() {
+        return "Salut je m'appelle "+this.name+"."
+    }
+
+
+}
+
+const maxime = new Person("Maxime");
+const teo = new Person("Téo");
+
+console.log(maxime.hello());
+console.log(teo.hello());
+
+console.log(Person.count)
+
+let monContact = new Contact()
+
+console.log(monContact)
+
+monContact.afficher();
+
+console.log(monContact.nom)
+
+monContact.prenom = "christophe"
+
+console.log(monContact.prenom)
+
+console.log("getter")
+console.log(monContact.name)
+
+console.log(monContact.nom)
+
+monContact.name = "Tata";
+
+console.log(monContact.name)
+
+// Héritage
+console.clear()
+
+
+class Programmer extends Person{
+    xp;
+    
+    hello() {
+        return super.hello()+ " et je fait du javascript :-)"
+    }
+    static drink(){
+        return "cofee";
+    }
+}
+
+const toto = new Programmer("Toto")
+console.log(toto.hello())
+
+// static
+
+console.log(Programmer.drink())
+
+console.log(Person.count)
--- 14 - Javascript/Demos/08_POO/secret_key.js
+++ 14 - Javascript/Demos/08_POO/secret_key.js
@@ -4,5 +4,8 @@ const maFonction = function(){
     return "my super function"
 }
 
+ const maVaribale = "ma secret key"
+
 
 export { MY_SERCRET_KEY, maFonction}
+export default maVaribale;
