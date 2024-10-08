805416aaff5f76a19425ffb7c63443bb6fa2d7bb
PayenThibaud <thibaudpayenbts@gmail.com>
2024-10-08 11:36:53 +0200

    push exo 23 JS

--- Thibaud_exo/Exo_JavaScript/Exo23/javaScript/index.js
+++ Thibaud_exo/Exo_JavaScript/Exo23/javaScript/index.js
@@ -1,7 +1,49 @@
-function afficherMMM(tableau) {
+// Tableau des étudiants
+let etudiants = [
+    {
+        prenom: "José",
+        nom: "Garcia",
+        matieres: {
+            francais: 16,
+            anglais: 7,
+            humour: 14
+        }
+    },
+    {
+        prenom: "Antoine",
+        nom: "De Caunes",
+        matieres: {
+            francais: 15,
+            anglais: 6,
+            humour: 16,
+            informatique: 4,
+            sport: 10
+        }
+    }
+];
 
-    let max = 0;
-    let min = 20;
-    let moyen = 0;
+function affichage(etudiants) {
+    etudiants.forEach(etudiant => {
 
-    tableau.forEach();
+        console.log("   Début   etudiant  ")
+
+        console.log("Etudiant: " + etudiant.prenom + " " + etudiant.nom);
+
+        let total = 0;
+        let nbNote = 0;
+
+        for (let matiere in etudiant.matieres) {
+            console.log("  " + matiere + " = " + etudiant.matieres[matiere]);
+            total += etudiant.matieres[matiere];
+            nbNote++;
+        }
+
+        let moyenne = total / nbNote;
+
+        console.log("Moyenne: " + moyenne);
+
+        console.log("   Fin   etudiant  ")
+    });
+}
+
+affichage(etudiants);
