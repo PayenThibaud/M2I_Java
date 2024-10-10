57b60b28e53db108ecb53eacc4e2b017a0245fdf
Christophe D. <christophe.delory@labom2iformation.fr>
2024-10-10 11:24:06 +0200

    correction exercice 26

---
+++ 14 - Javascript/Exercices/exercice26/js/classe/contact.js
@@ -0,0 +1,10 @@
+export default class Contact {
+    constructor(n,p,t,e,te,d){
+        this.titre = t;
+        this.nom = n;
+        this.prenom = p;
+        this.telephone = te;
+        this.email = e;
+        this.dateNaissance = d;
+    }
+}
