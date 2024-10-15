05b187cd807708542ad53f5f30c7130e19be1c68
PayenThibaud <Thibaudpayenbts@gmail.com>
2024-10-15 16:49:06 +0200

    ajout css et navbar

--- Thibaud_exo/exo_Typescript/Angular1/AngularExo1.1/src/app/app.routes.ts
+++ Thibaud_exo/exo_Typescript/Angular1/AngularExo1.1/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {SeriesComponent} from './component/series/series.component';
+import {CounterComponent} from './component/counter/counter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: SeriesComponent},
+  {path: 'counter', component: CounterComponent}
+];
--- Thibaud_exo/exo_Typescript/Angular1/AngularExo1.1/src/app/component/series/series.component.ts
+++ Thibaud_exo/exo_Typescript/Angular1/AngularExo1.1/src/app/component/series/series.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './series.component.css'
 })
 export class SeriesComponent {
+  listSeries: string[] = [
+    'Breaking Bad',
+    'Stranger Things',
+    'Game of Thrones',
+    'The Witcher',
+    'Money Heist'
+  ];
 
+  delete(i: number): void {
+    if (i >= 0 && i < this.listSeries.length) {
+      this.listSeries.splice(i, 1);
+    }
+  }
 }
