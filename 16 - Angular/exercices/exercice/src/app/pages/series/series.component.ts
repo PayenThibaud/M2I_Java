675af5c637f0e32bbe6c7d21bfa87d0d35e1d995
Christophe R <christophe.r@utopios.solutions>
2024-10-16 14:43:38 +0200

    Angular - Correction exercice 4

--- 16 - Angular/exercices/exercice/src/app/pages/series/series.component.ts
+++ 16 - Angular/exercices/exercice/src/app/pages/series/series.component.ts
@@ -14,7 +14,8 @@ export class SeriesComponent {
   series: string[] = ["Série 1", "Série 2", "Série 3"]
 
   removeSerie(index : number) : void {
-    this.series.splice(index, 1)
+    // this.series.splice(index, 1)
+    this.series = this.series.filter((_, i) => i != index)
   }
 
   order: order = 'asc'
