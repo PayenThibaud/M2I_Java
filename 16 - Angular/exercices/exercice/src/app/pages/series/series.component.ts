cd5d10893d06db3f383e0ba0d946db44f0772376
Christophe R <christophe.r@utopios.solutions>
2024-10-16 11:59:38 +0200

    Angular - correction exercice 3

--- 16 - Angular/exercices/exercice/src/app/pages/series/series.component.ts
+++ 16 - Angular/exercices/exercice/src/app/pages/series/series.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SortPipe } from '../../utils/pipes/sort.pipe';
+
+type order = 'asc' | 'desc'
 
 @Component({
   selector: 'app-series',
   standalone: true,
-  imports: [],
+  imports: [SortPipe],
   templateUrl: './series.component.html',
   styleUrl: './series.component.css'
 })
@@ -12,5 +15,11 @@ export class SeriesComponent {
 
   removeSerie(index : number) : void {
     this.series.splice(index, 1)
+  }
+
+  order: order = 'asc'
+
+  setOrder(order : order) : void {
+    this.order = order
   }
 }
