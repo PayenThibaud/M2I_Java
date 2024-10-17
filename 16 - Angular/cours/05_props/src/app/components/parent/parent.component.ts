033857802f83542ba4b254e784a39cfa12c571e2
Christophe R <christophe.r@utopios.solutions>
2024-10-17 16:12:03 +0200

    Angular - Demo les props

---
+++ 16 - Angular/cours/05_props/src/app/components/parent/parent.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { EnfantComponent } from '../enfant/enfant.component';
+
+interface User {
+  nom: string;
+  age: number
+}
+
+
+@Component({
+  selector: 'app-parent',
+  standalone: true,
+  imports: [EnfantComponent],
+  templateUrl: './parent.component.html',
+  styleUrl: './parent.component.css'
+})
+export class ParentComponent {
+  parentMessage: string = "Hello depuis le composant parent !"
+
+  // user: User = {
+  //   nom: "toto",
+  //   age: 30
+  // }
+
+  listUser: User[] = [
+    {
+      nom: "toto",
+      age: 30
+    },
+    {
+      nom: "tata",
+      age: 35
+    },
+    {
+      nom: "titi",
+      age: 50
+    }
+  ]
+}
