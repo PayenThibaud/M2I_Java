7f2de3b1945ffd570610f126311ea4e7c7154be0
PayenThibaud <Thibaudpayenbts@gmail.com>
2024-10-17 11:34:49 +0200

    angular

--- Thibaud_exo/exo_Typescript/Angular1/AngularExo1.1/src/app/component/librairie/librairie.component.ts
+++ Thibaud_exo/exo_Typescript/Angular1/AngularExo1.1/src/app/component/librairie/librairie.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {PoucePipe} from '../../utils/pipes/pouce.pipe';
+import {FormsModule} from '@angular/forms';
 
 interface Book {
   titre: string;
@@ -11,21 +12,45 @@ interface Book {
   selector: 'app-librairie',
   standalone: true,
   imports: [
-    PoucePipe
+    PoucePipe,
+    FormsModule
   ],
   templateUrl: './librairie.component.html',
   styleUrl: './librairie.component.css'
 })
 export class LibrairieComponent {
   livres: Book[] = [
-    { titre: 'livre1', auteur: '1', isRead: false },
-    { titre: 'livre2', auteur: '2', isRead: false },
-    { titre: 'livre3', auteur: '3', isRead: false },
-    { titre: 'livre4', auteur: '4', isRead: false },
-    { titre: 'livre5', auteur: '5', isRead: false }
+    {titre: 'livre1', auteur: '1', isRead: false},
+    {titre: 'livre2', auteur: '2', isRead: false},
+    {titre: 'livre3', auteur: '3', isRead: false},
+    {titre: 'livre4', auteur: '4', isRead: false},
+    {titre: 'livre5', auteur: '5', isRead: false}
   ];
 
   toggleIsRead(livre: Book) {
     livre.isRead = !livre.isRead;
+  }
+
+  livre: Book = {
+    titre: '',
+    auteur: '',
+    isRead: false
+  }
+
+  submitLivre() {
+    if (this.livre.titre && this.livre.auteur) {
+      this.livres.push({...this.livre});
+      this.resetForm();
+    }
+  }
+
+  resetForm() {
+    this.livre = {
+      titre: '',
+      auteur: '',
+      isRead: false
+    };
+
+
   }
 }
